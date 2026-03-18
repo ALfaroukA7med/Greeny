82448afb43cbc92e8dc8a2f0ea418888baa77dc6
ALfaroukA7med <alfaroukahmed99946@gmail.com>
2026-03-19 01:31:45 +0200

    Hero Section Added

--- front-end/JavaScript/script.js
+++ front-end/JavaScript/script.js
@@ -1,8 +1,15 @@
 let hamburger = document.querySelector(".hamburger");
 let menu = document.querySelector(".menu");
-hamburger.onclick = function () {
+let nav = document.querySelector("nav").offsetHeight;
+// hero.style.cssText = `min-height: calc(100vh - ${nav}px);`;
+menu.style.top = `${nav}px`;
+document.body.style.paddingTop = `${nav}px`; // => to make the menu down the navbar
+hamburger.onclick = (_) => {
   menu.classList.toggle("show");
+  if (menu.classList.contains("show"))
+    document.body.style.paddingTop = `${nav + menu.offsetHeight}px`; // => to make the menu down the navbar
+  else document.body.style.paddingTop = `${nav}px`; // => to make the menu down the navbar
 };
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", (_) => {
   hamburger.classList.toggle("is-active");
 });
