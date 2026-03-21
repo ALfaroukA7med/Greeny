8f257af5152d0b481f076b11da5b34c37ef34c8d
ALfaroukA7med <alfaroukahmed99946@gmail.com>
2026-03-21 02:22:33 +0200

    Featured Products added

--- front-end/JavaScript/script.js
+++ front-end/JavaScript/script.js
@@ -1,14 +1,11 @@
 let hamburger = document.querySelector(".hamburger");
 let menu = document.querySelector(".menu");
 let nav = document.querySelector("nav").offsetHeight;
-// hero.style.cssText = `min-height: calc(100vh - ${nav}px);`;
-menu.style.top = `${nav}px`;
+let hero = document.querySelector('.hero-section');
+hero.style.cssText = `min-height: calc(100vh - ${nav}px);`;
 document.body.style.paddingTop = `${nav}px`; // => to make the menu down the navbar
 hamburger.onclick = (_) => {
   menu.classList.toggle("show");
-  if (menu.classList.contains("show"))
-    document.body.style.paddingTop = `${nav + menu.offsetHeight}px`; // => to make the menu down the navbar
-  else document.body.style.paddingTop = `${nav}px`; // => to make the menu down the navbar
 };
 hamburger.addEventListener("click", (_) => {
   hamburger.classList.toggle("is-active");
