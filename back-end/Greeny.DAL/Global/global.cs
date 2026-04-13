57836d9c471b3269c6c1fcb63eb9cb2100801336
Mo7ammed Sayed <mo7ammedsayed4@gmail.com>
2026-04-13 17:28:09 +0200

    Remove old back-end to replace with the new one

--- back-end/Greeny.DAL/Global/global.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Greeny.Models;
-global using Microsoft.AspNetCore.Identity;
-global using Greeny.DAL.Entities;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Metadata.Builders;
-global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
