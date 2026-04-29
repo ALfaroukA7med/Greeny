37d3f960af06e5e8f871e9cf0944068e968664c3
ahmed montaser <ahmeduabd67@gmail.com>
2026-04-29 12:15:53 +0300

    shut up ya farouk

--- front-end/JavaScript/community.js
+++ front-end/JavaScript/community.js
@@ -17,3 +17,14 @@ likeIcons.forEach((icon) => {
     countNode.textContent = ` ${currentCount}`;
   });
 });
+
+const upload = document.getElementById("photoUpload");
+const fileName = document.getElementById("fileName");
+
+upload.addEventListener("change", function () {
+  if (this.files.length > 0) {
+    fileName.textContent = this.files[0].name;
+  } else {
+    fileName.textContent = "No file chosen";
+  }
+});
