7502d5a45fa83b29066df679b3d053988f140ea1
Mo7ammed Sayed <mo7ammedsayed4@gmail.com>
2026-05-23 08:29:40 +0300

    Add review View

--- back-end/Greeny.BIL/Errors/ReviewError.cs
+++ back-end/Greeny.BIL/Errors/ReviewError.cs
@@ -10,6 +10,11 @@ namespace Greeny.BLL.Errors
 
         public static Error InvalidData
     = new Error("Review.InvalidData", "Invalid review data", ErrorType.BadRequest);
+
+        public static Error DuplicateReview
+    = new Error("Review.DuplicateReview", "You have already reviewed this product", ErrorType.BadRequest);
+
+        public static Error UserNotFound = new Error("Review.UserNotFound", "User not found", ErrorType.NotFound);
     }
 
 
